a797019925ae42ad6bdd0770f747e836d652f14e
Dmitrii Kochkaev <128289789+kochkaev@users.noreply.github.com>
2024-12-02 16:08:36 +0700

    Update NewAccountData.kt

--- src/main/kotlin/ru/kochkaev/zixamc/requests/dataclassSQL/NewAccountData.kt
+++ src/main/kotlin/ru/kochkaev/zixamc/requests/dataclassSQL/NewAccountData.kt
@@ -1,7 +1,7 @@
 namespace ru.kochkaev.zixamc.requests.dataclassSQL
 
 data class NewAccountData (
-    val minecraft_accounts: List<MinecraftAccountData>
-    val requests: List<RequestData>
-    val agreed_with_rules: Boolean = false
+    val minecraft_accounts: List<MinecraftAccountData>,
+    val requests: List<RequestData>,
+    val agreed_with_rules: Boolean = false,
 ) : AccountData()
